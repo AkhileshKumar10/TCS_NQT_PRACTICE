fa3dc6d0b2a46958071b80958fcaf8f3893f9195
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-04-13 16:11:07 +0530

    checking_anagrams.cpp

--- STRINGS_PRACTICE/checking_anagrams.cpp
+++ STRINGS_PRACTICE/checking_anagrams.cpp
@@ -2,16 +2,17 @@
 #include<algorithm>
 using namespace std;
 
-bool isanagram(string&str1 , string&str2){
+int  isanagram(string&str1 , string&str2){
+    int count=0;
     sort(str1.begin(),str1.begin());
     sort(str2.begin(),str2.end());
     for(int i =0; i <str1.size();i++){
         if(str1[i]==str2[i]){
-            return true;
+            count++;
         }
 
     }
-    return false;
+    return count;
 }
 
 int main(){
@@ -20,7 +21,7 @@ int main(){
     getline(cin,str1);
     cout<<"enter the string 2";
     getline(cin,str2);
-    bool result = isanagram(str1,str2);
+    int result = isanagram(str1,str2);
     cout<<" the result is "<<" "<<result<<endl;
 
 }
